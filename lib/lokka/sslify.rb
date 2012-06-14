e774cdb21380c87244c1b56160555b117894f2d4
SATO Naoya <s@tonaoya.com>
2012-06-14 22:19:56 +0900

    Add admin page

--- lib/lokka/sslify.rb
+++ lib/lokka/sslify.rb
@@ -1,13 +1,26 @@
 module Lokka
-  module SSLify
+  module Sslify
     def self.registered(app)
       app.configure :production do
         app.before do
-          unless request.secure?
-            url = request.url.sub('http', 'https')
-            redirect url
+          sslify = Option.sslify || false
+          if sslify
+            unless request.secure?
+              url = request.url.sub('http', 'https')
+              redirect url
+            end
           end
         end
+      end
+
+      app.get '/admin/plugins/sslify' do
+        haml :"plugin/lokka-sslify/views/index", :layout => :"admin/layout"
+      end
+
+      app.put '/admin/plugins/sslify' do
+        Option.sslify = params['sslify']
+        flash[:notice] = 'Updated'
+        redirect '/admin/plugins/sslify'
       end
     end
   end
